31e38da44fbb4950faffbc8052619dab86645f12
unknown <phff800@gmail.com>
2024-02-26 14:12:05 -0300

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,14 +16,14 @@ export default function Home(){
                  exit={{x:window.innerWidth}}
                  >
                     <section className="w-screen p-3 text-center ">
-                    <h2 className="font-semibold text-3xl lg:text-4xl">Está procurando um lugar para consultar a <span className="text-red-600">FIPE</span> do seu veículo?</h2>
+                    <h2 className="font-semibold text-xl sm:text-2xl lg:text-3xl">Está procurando um lugar para consultar a <span className="text-red-600">FIPE</span> do seu veículo?</h2>
                     </section>
-                    <section className="grid grid-cols-2 gap-5 p-3 mt-12 m-auto">
+                    <section className="grid grid-cols-2 gap-5 p-3 mt- m-auto">
                         <Card text={"Custo zero"} svg={money}/>
                         <Card text={"Fipe atualizada"} svg={actual}/>
                     </section>
                     <div className="w-scren my-7 text-center">
-                        <h2 className="text-4xl font-bold">Encontrou o lugar certo!</h2>
+                        <h2 className="text-3xl font-bold">Encontrou o lugar certo!</h2>
                     </div>
                     <div className="flex justify-center">
                         <Link to={'/querys'}><button className="bg-cyan-600 py-4 px-10 sm:px-20 mb-10 font-bold text-white uppercase rounded-xl hover:text-black hover:opacity-85">Consulte agora</button></Link>
