6e30444a5bd560d96c3e5e4083be3abdb31e005a
unknown <phff800@gmail.com>
2024-02-26 14:31:12 -0300

    bug

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,7 @@ export default function Footer(){
         <footer className="w-screen mt-5 fixed bottom-9 text-center ">
             <h1 
             className="font-bold text-xs text-center md:text-lg">
-                Copyright © 2024 by <a className="text-red-700 " href="https:www.linkedin.com/in/felipefariasdev" 
-                target="_blank" >Felipe Farias</a> All rights reserved.</h1>
+                Copyright © 2024 by <span className="text-red-600">Felipe Farias.</span> All rights reserved.</h1>
         </footer>
     )
 }
