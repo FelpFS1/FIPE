1f5b2db09ea40495abce7484db18dbc8135611e4
unknown <phff800@gmail.com>
2024-02-26 14:55:10 -0300

    fix responsive design

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,7 @@ type CardProps ={
 }
 export default function Card({text,svg}:CardProps){
     return(
-    <div className='flex flex-col items-center m-auto justify-center w-full lg:w-[70%] lg:h-96 text-center bg-cyan-600 text-slate-50 font-poppins p-5 rounded-3xl'>
+    <div className='flex flex-col items-center m-auto justify-center w-full lg:w-[70%] lg:h-60 xl:h-96 text-center bg-cyan-600 text-slate-50 p-5 rounded-3xl'>
             <h4 className="font-bold text-xs md:text-lg lg:text-3xl xl:6xl mb-5">{text}</h4>
             <div className="border-8 rounded-3xl p-5">
                 <img className='w-10 h-[76px] lg:w-20' src={svg} alt="" />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logo from '../assets/fipe.png'
 
 export default function Header(){
     return(
-        <header className='w-screen p-2 lg:h-40' >
+        <header className='w-screen p-2  lg:h-28' >
             <div className='w-40 m-auto lg:w-60'>
                 <img src={logo} alt="logo fipe"/>
             </div>
