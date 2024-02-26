b256b6b6a5a6d1337331d9d180bcbf32f4dba451
unknown <phff800@gmail.com>
2024-02-26 14:33:37 -0300

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
 export default function Footer(){
     return(
         <footer className="w-screen mt-5 fixed bottom-9 text-center ">
-            <h1 
-            className="font-bold text-xs text-center md:text-lg">
-                Copyright © 2024 by <span className="text-red-600">Felipe Farias.</span> All rights reserved.</h1>
+            <h1 className="font-bold text-xs text-center md:text-lg">Copyright © 2024 by <span className="text-red-600">Felipe Farias.</span> All rights reserved.</h1>
         </footer>
     )
 }
